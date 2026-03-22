75743420f62864ce7c368606ff31a675333b995c
Tanvi Maniya <139962265+tanvimaniya3@users.noreply.github.com>
2026-03-22 19:14:32 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,66 +1,31 @@
-let params = new URLSearchParams(window.location.search);
-let id = params.get("id");
-
 fetch("https://ecommerce-website-pmr7.onrender.com/api/products")
 .then(res => res.json())
 .then(data => {
 
-let product = data.find(p => p._id == id || p.id == id);
+let box = document.getElementById("productContainer");
 
-// 🔴 safety check
-if(!product){
-alert("Product not found");
-return;
-}
+if(!box) return;
 
-// ✅ correct ID use
-document.getElementById("mainImage").src = product.image;
-document.getElementById("pName").innerText = product.name;
-document.getElementById("pPrice").innerText = "₹" + product.price;
+box.innerHTML = "";
 
-// 👉 save globally
-window.currentProduct = product;
+data.forEach(p => {
 
-updateCartCount();
+box.innerHTML += `
+<div class="product">
+
+<a href="product.html?id=${p._id}">
+<img src="${p.image}" width="200">
+<h3>${p.name}</h3>
+</a>
+
+<p>₹${p.price}</p>
+
+</div>
+`;
 
 });
 
-
-// ADD TO CART
-function addToCart(){
-
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-cart.forEach(item=>{
-if(!item.qty){
-item.qty = 1;
-}
+})
+.catch(err => {
+console.log("PRODUCT ERROR:", err);
 });
-
-let existing = cart.find(p => p._id == window.currentProduct._id);
-
-if(existing){
-existing.qty += 1;
-}else{
-window.currentProduct.qty = 1;
-cart.push(window.currentProduct);
-}
-
-localStorage.setItem("cart", JSON.stringify(cart));
-
-alert("Product Added To Cart");
-
-updateCartCount();
-
-}
-
-
-// CART COUNT
-function updateCartCount(){
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-let badge = document.getElementById("cartCount");
-
-if(badge){
-badge.innerText = cart.length;
-}
-}
