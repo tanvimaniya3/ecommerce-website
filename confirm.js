a7526c0f98b7e51270ccc1a2d5590097779cc213
Tanvi Maniya <139962265+tanvimaniya3@users.noreply.github.com>
2026-03-28 17:49:42 +0530

    Update confirm.js

--- confirm.js
+++ confirm.js
@@ -1,18 +1,23 @@
 let order = JSON.parse(localStorage.getItem("lastOrder"));
 
-// customer details
+if(!order){
+document.body.innerHTML = "<h2>No Order Found ❌</h2>";
+}
+
+// details
 document.getElementById("cName").innerText = "Name: " + order.name;
 document.getElementById("cPhone").innerText = "Phone: " + order.phone;
-document.getElementById("cAddress").innerText = "Address: " + order.address + ", " + order.state + " - " + order.pincode;
+document.getElementById("cAddress").innerText =
+"Address: " + order.address + ", " + order.state + " - " + order.pincode;
 
-// products
+// items
 let box = document.getElementById("orderItems");
 
 order.items.forEach(p => {
 
 box.innerHTML += `
 <div>
-<img src="${p.image}" width="80">
+<img src="https://ecommerce-website-pmr7.onrender.com${p.image}" width="80">
 <h4>${p.name}</h4>
 <p>₹${p.price} x ${p.qty}</p>
 </div>
