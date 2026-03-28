f671d2b906ddff71c3f7e053960e4a3c597ec94c
Tanvi Maniya <139962265+tanvimaniya3@users.noreply.github.com>
2026-03-28 18:43:29 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,34 @@
+document.getElementById("orderForm").addEventListener("submit", async function(e){
 
+e.preventDefault();
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+let order = {
+name: document.getElementById("name").value,
+phone: document.getElementById("phone").value,
+pincode: document.getElementById("pincode").value,
+state: document.getElementById("state").value,
+address: document.getElementById("address").value,
+items: cart,
+date: new Date().toLocaleString(),
+status: "Pending"
+};
+
+localStorage.setItem("lastOrder", JSON.stringify(order));
+
+await fetch("https://ecommerce-website-pmr7.onrender.com/api/orders",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body: JSON.stringify(order)
+});
+
+alert("Order Placed ✅");
+
+localStorage.removeItem("cart");
+
+window.location.href = "confirm.html";
+
+});
