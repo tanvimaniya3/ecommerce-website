5fc7ea50c77c8046a38c3563977288506e9b6e7f
Tanvi Maniya <139962265+tanvimaniya3@users.noreply.github.com>
2026-03-28 17:47:00 +0530

    Update product.js

--- product.js
+++ product.js
@@ -4,16 +4,22 @@ let id = params.get("id");
 let productData = null;
 let quantity = 1;
 
-// 🔥 LOAD PRODUCT
 fetch("https://ecommerce-website-pmr7.onrender.com/api/products")
 .then(res=>res.json())
 .then(data=>{
 
 let product = data.find(p => p._id == id);
 
+if(!product){
+alert("Product nahi mila ❌");
+return;
+}
+
 productData = product;
 
-document.getElementById("pImage").src = "https://ecommerce-website-pmr7.onrender.com" + product.image;
+document.getElementById("pImage").src =
+"https://ecommerce-website-pmr7.onrender.com" + product.image;
+
 document.getElementById("pName").innerText = product.name;
 document.getElementById("pPrice").innerText = "₹" + product.price;
 document.getElementById("pDesc").innerText = product.description || "";
@@ -22,7 +28,7 @@ updateCartCount();
 
 });
 
-// 🔥 QTY
+// QTY
 function increaseQty(){
 quantity++;
 document.getElementById("qty").innerText = quantity;
@@ -35,8 +41,13 @@ document.getElementById("qty").innerText = quantity;
 }
 }
 
-// 🔥 ADD TO CART
+// ADD TO CART
 function addToCart(){
+
+if(!productData){
+alert("Product load nahi hua ❌");
+return;
+}
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
@@ -54,25 +65,28 @@ localStorage.setItem("cart", JSON.stringify(cart));
 alert("Added to Cart");
 
 updateCartCount();
-
 }
 
-// 🔥 BUY NOW
+// BUY NOW
 function buyNow(){
+
+if(!productData){
+alert("Product load nahi hua ❌");
+return;
+}
 
 let cart = [];
 
-productData.qty = 1;
+productData.qty = quantity;
 
 cart.push(productData);
 
 localStorage.setItem("cart", JSON.stringify(cart));
 
 window.location.href = "checkout.html";
-
 }
 
-// 🔥 CART COUNT
+// CART COUNT
 function updateCartCount(){
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
