e03fb6e7527747591579fa9f5eafecb44d2ee518
Tanvi Maniya <139962265+tanvimaniya3@users.noreply.github.com>
2026-03-31 17:38:58 +0530

    Update products.js

--- public/products.js
+++ public/products.js
@@ -1,10 +1,23 @@
 let allProducts = [];
 
+let params = new URLSearchParams(window.location.search);
+let searchQuery = params.get("search");
+
 fetch("https://ecommerce-website-1-s0j9.onrender.com/api/products")
 .then(res => res.json())
 .then(data => {
+
 allProducts = data;
+
+if(searchQuery){
+let filtered = data.filter(p =>
+p.name.toLowerCase().includes(searchQuery.toLowerCase())
+);
+showProducts(filtered);
+}else{
 showProducts(data);
+}
+
 });
 
 function showProducts(products){
@@ -12,6 +25,11 @@ let box = document.getElementById("productContainer");
 if(!box) return;
 
 box.innerHTML = "";
+
+if(products.length === 0){
+box.innerHTML = "<h3>No Product Found ❌</h3>";
+return;
+}
 
 products.forEach(p => {
 box.innerHTML += `
@@ -26,7 +44,6 @@ box.innerHTML += `
 });
 }
 
-// 🔍 SEARCH
 function searchProduct(){
 let input = document.getElementById("searchInput").value.toLowerCase();
 
