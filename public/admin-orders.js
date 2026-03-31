6a1a0ce28f660ce72ab088a847cf514ad22831d5
Tanvi Maniya <139962265+tanvimaniya3@users.noreply.github.com>
2026-03-31 12:49:11 +0530

    Update admin-orders.js

--- public/admin-orders.js
+++ public/admin-orders.js
@@ -1,64 +1,39 @@
 async function loadOrders(){
-
-let res = await fetch("https://ecommerce-website-pmr7.onrender.com/api/orders");
+let res = await fetch("https://ecommerce-website-1-s0j9.onrender.com/api/orders");
 let orders = await res.json();
 
 let box = document.getElementById("ordersBox");
 box.innerHTML = "";
 
-// 🔥 latest first
 orders.reverse();
 
-if(orders.length === 0){
-box.innerHTML = "<h3>No Orders Yet ❌</h3>";
-return;
-}
-
 orders.forEach(order => {
-
 box.innerHTML += `
-
-<div class="order" style="border:1px solid #ccc; padding:10px; margin:10px; border-radius:10px;">
-
-<h3>👤 ${order.name}</h3>
-<p>📞 ${order.phone}</p>
-<p>📍 ${order.address}, ${order.state} - ${order.pincode}</p>
-
-<h4>🛒 Products:</h4>
+<div class="order">
+<h3>${order.name}</h3>
 
 ${order.items.map(p => `
-<div style="display:flex; gap:10px; align-items:center;">
-<img src="https://ecommerce-website-pmr7.onrender.com${p.image}" width="60">
 <div>
-${p.name}<br>
-₹${p.price} x ${p.qty}
-</div>
+<img src="https://ecommerce-website-1-s0j9.onrender.com${p.image}" width="60">
+${p.name} - ₹${p.price} x ${p.qty}
 </div>
 `).join("")}
 
-<br>
-
-<p><b>Status:</b> ${order.status}</p>
+<p>Status: ${order.status}</p>
 
 <select onchange="updateStatus('${order._id}', this.value)">
-<option value="Pending" ${order.status==="Pending"?"selected":""}>Pending</option>
-<option value="Shipped" ${order.status==="Shipped"?"selected":""}>Shipped</option>
-<option value="Delivered" ${order.status==="Delivered"?"selected":""}>Delivered</option>
+<option ${order.status==="Pending"?"selected":""}>Pending</option>
+<option ${order.status==="Shipped"?"selected":""}>Shipped</option>
+<option ${order.status==="Delivered"?"selected":""}>Delivered</option>
 </select>
 
 </div>
-
 `;
-
 });
-
 }
 
-
-// 🔥 UPDATE STATUS
 async function updateStatus(id, status){
-
-await fetch("https://ecommerce-website-pmr7.onrender.com/api/orders/" + id,{
+await fetch("https://ecommerce-website-1-s0j9.onrender.com/api/orders/" + id,{
 method:"PUT",
 headers:{
 "Content-Type":"application/json"
@@ -66,11 +41,8 @@ headers:{
 body: JSON.stringify({status})
 });
 
-alert("Status Updated ✅");
-
+alert("Updated");
 loadOrders();
-
 }
 
-// पहली बार load
 loadOrders();
