e3ebc660858b5f4ee73bb5514139365af60e9817
Tanvi Maniya <139962265+tanvimaniya3@users.noreply.github.com>
2026-03-31 12:49:51 +0530

    Update products.js

--- public/products.js
+++ public/products.js
@@ -1,56 +1,33 @@
 let allProducts = [];
 
-// 🔥 LOAD PRODUCTS
-fetch("https://ecommerce-website-pmr7.onrender.com/api/products")
+fetch("https://ecommerce-website-1-s0j9.onrender.com/api/products")
 .then(res => res.json())
 .then(data => {
-
-allProducts = data; // save full data
+allProducts = data;
 showProducts(data);
-
-})
-.catch(err => {
-console.log("PRODUCT ERROR:", err);
 });
 
-
-// 🔥 SHOW PRODUCTS
 function showProducts(products){
-
 let box = document.getElementById("productContainer");
-
 if(!box) return;
 
 box.innerHTML = "";
 
-if(products.length === 0){
-box.innerHTML = "<h3>No Product Found ❌</h3>";
-return;
-}
-
 products.forEach(p => {
-
 box.innerHTML += `
 <div class="product">
-
 <a href="product.html?id=${p._id}">
-<img src="https://ecommerce-website-pmr7.onrender.com${p.image}" width="200">
+<img src="https://ecommerce-website-1-s0j9.onrender.com${p.image}" width="200">
 <h3>${p.name}</h3>
 </a>
-
 <p>₹${p.price}</p>
-
 </div>
 `;
-
 });
-
 }
 
-
-// 🔍 SEARCH FUNCTION
+// 🔍 SEARCH
 function searchProduct(){
-
 let input = document.getElementById("searchInput").value.toLowerCase();
 
 let filtered = allProducts.filter(p =>
@@ -58,5 +35,4 @@ p.name.toLowerCase().includes(input)
 );
 
 showProducts(filtered);
-
 }
