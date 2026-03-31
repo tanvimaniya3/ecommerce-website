fcdbbb27e15091accdbe5cfcfbeee2e8a54ad24c
Tanvi Maniya <139962265+tanvimaniya3@users.noreply.github.com>
2026-03-31 12:50:21 +0530

    Update product.js

--- public/product.js
+++ public/product.js
@@ -4,28 +4,22 @@ let id = params.get("id");
 let productData = null;
 let quantity = 1;
 
-fetch("https://ecommerce-website-pmr7.onrender.com/api/products")
+fetch("https://ecommerce-website-1-s0j9.onrender.com/api/products")
 .then(res=>res.json())
 .then(data=>{
 
 let product = data.find(p => p._id == id);
 
-if(!product){
-alert("Product nahi mila ❌");
-return;
-}
-
 productData = product;
 
 document.getElementById("pImage").src =
-"https://ecommerce-website-pmr7.onrender.com" + product.image;
+"https://ecommerce-website-1-s0j9.onrender.com" + product.image;
 
 document.getElementById("pName").innerText = product.name;
 document.getElementById("pPrice").innerText = "₹" + product.price;
 document.getElementById("pDesc").innerText = product.description || "";
 
 updateCartCount();
-
 });
 
 // QTY
@@ -43,12 +37,6 @@ document.getElementById("qty").innerText = quantity;
 
 // ADD TO CART
 function addToCart(){
-
-if(!productData){
-alert("Product load nahi hua ❌");
-return;
-}
-
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 let existing = cart.find(p => p._id == productData._id);
@@ -61,46 +49,29 @@ cart.push(productData);
 }
 
 localStorage.setItem("cart", JSON.stringify(cart));
-
 alert("Added to Cart");
-
 updateCartCount();
 }
 
 // BUY NOW
 function buyNow(){
-
-if(!productData){
-alert("Product load nahi hua ❌");
-return;
-}
-
 let cart = [];
-
 productData.qty = quantity;
-
 cart.push(productData);
 
 localStorage.setItem("cart", JSON.stringify(cart));
-
 window.location.href = "checkout.html";
 }
 
 // CART COUNT
 function updateCartCount(){
-
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 let count = 0;
-
 cart.forEach(p=>{
 count += p.qty || 1;
 });
 
 let badge = document.getElementById("cartCount");
-
-if(badge){
-badge.innerText = count;
-}
-
+if(badge) badge.innerText = count;
 }
